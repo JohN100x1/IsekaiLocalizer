1eb90a146ed665a65320995e772b468b01a23600
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-04-23 19:27:37 +0100

    feat: add indent to json

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import json
 import re
 from pathlib import Path
 
-from msgspec import to_builtins
 from msgspec.json import decode as json_decode
 from msgspec.json import encode as json_encode
+from msgspec.json import format as json_format
 
 from api import ChatAPI, OraAPI
 from models import LocalizationPack, LocalizedString
@@ -23,10 +22,10 @@ class PackLocalizer:
             return json_decode(file.read(), type=LocalizationPack)
 
     @staticmethod
-    def save(path: Path, pack: LocalizationPack):
+    def save(path: Path, pack: LocalizationPack, indent: int = 2):
         """Save the localization pack at specified path."""
-        with open(path, "w") as file:
-            file.write(json.dumps(to_builtins(pack)))
+        with open(path, "wb") as file:
+            file.write(json_format(json_encode(pack), indent=indent))
 
     @staticmethod
     def __get_prompt(entries_json: str) -> str:
@@ -62,7 +61,11 @@ class PackLocalizer:
         return pack_translated
 
     def localize(
-        self, path: Path, replace: bool = False, batch_size: int = 16
+        self,
+        path: Path,
+        replace: bool = False,
+        batch_size: int = 16,
+        indent: int = 2,
     ):
         """Localize the localization pack from path and save it."""
         pack = self.load(path)
@@ -72,7 +75,7 @@ class PackLocalizer:
             save_path = path
         else:
             save_path = path.with_name(f"{path.stem}Translated{path.suffix}")
-        self.save(save_path, pack_translated)
+        self.save(save_path, pack_translated, indent=indent)
 
 
 if __name__ == "__main__":
