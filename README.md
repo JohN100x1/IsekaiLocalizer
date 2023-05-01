f1b0b05307ffca7ef770a3bc6028786013e8c5fa
JohN100x1 <JohN100x1@hotmail.co.uk>
2023-05-01 13:36:43 +0100

    feat: add cli command

--- README.md
+++ README.md
@@ -50,11 +50,11 @@ Requirements:
 - [Python 3.11](https://www.python.org/downloads/) or above
 - [Poetry](https://python-poetry.org/docs/#installation)
 
-1. Place your `LocalizationPack.json` in the `data` folder.
-2. Install dependencies using `poetry install` in your terminal.
-3. Create Open AI account and get your access token from here https://chat.openai.com/api/auth/session.
-4. Rename `example.env` to `.env` and set `OPENAI_ACCESS_TOKEN` to your access token.
-5. Run `main.py` using `poetry run python src/main.py` in your terminal.
+1. Install dependencies using `poetry install` in your terminal.
+2. Create Open AI account and get your access token from here https://chat.openai.com/api/auth/session.
+3. Rename `example.env` to `.env` and set `OPENAI_ACCESS_TOKEN` to your access token.
+4. Run `main.py` using `poetry run python src/main.py PATH_TO_YOUR_JSON_PACK` in your terminal where
+`PATH_TO_YOUR_JSON_PACK` is the path your localization pack.
 
 ## TODO
 - Address Bad translations for long text
--- src/main.py
+++ src/main.py
@@ -1,25 +1,27 @@
 from pathlib import Path
 
+import click
+
 from api.openai import OpenAIAPI
 from pack_processors import PackSerializer, PackTranslator
 
-if __name__ == "__main__":
-    replace: bool = True
-    json_indent: int = 2
 
-    path_pack = Path(__file__).parent.parent / "data" / "LocalizationPack.json"
-    # path_pack = Path(__file__).parent.parent / "data" / "ProblematicPack.json"
-    # path_pack = Path(__file__).parent.parent / "data" / "TestPack.json"
+@click.command(help="Add missing localization to LocalizationPack.json")
+@click.argument("path", type=click.Path(exists=True))
+@click.option("--json_indent", "-i", type=int, default=2, help="json indent")
+def main(path: Path, json_indent: int = 2):
+    """Add missing localization to LocalizationPack.json"""
+
     translator_api = OpenAIAPI.create()
     translator = PackTranslator(translator_api)
     serializer = PackSerializer()
 
-    pack_original = serializer.load(path_pack)
+    pack_original = serializer.load(path)
     pack_translated = translator.translate(pack_original)
-    if replace:
-        save_path = path_pack
-    else:
-        save_path = path_pack.with_name(
-            f"{path_pack.stem}Translated{path_pack.suffix}"
-        )
-    serializer.save(save_path, pack_translated, indent=json_indent)
+    serializer.save(path, pack_translated, indent=json_indent)
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO: cycling between access tokens
